e8836f5454c18b0fc582ba716cbb6d438ec75191
ekrem <yalcinekrem89@gmail.com>
2023-01-04 15:45:02 +0300

    Initial Commit

---
+++ RickAndMortyDataBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyDataBase
+//
+//  Created by Tayyip on 4.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
